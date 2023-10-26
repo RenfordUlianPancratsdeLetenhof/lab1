e140ecd7eedff935411768a0dcf912d71ef9789e
RenfordUlianPancratsdeLetenhof <berez_sasha@ukr.net>
2023-10-26 12:42:29 +0300

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ans_polyminal_with_x_button_Click(object sender, RoutedEventArgs e)
+        {
+            Polyminal polyminal;
+
+            double a, b, c, d, x;
+
+            try
+            {
+                a = Convert.ToDouble(a_textbox_1.Text);
+                b = Convert.ToDouble(b_textbox_1.Text);
+                c = Convert.ToDouble(c_textbox_1.Text);
+                d = Convert.ToDouble(d_textbox_1.Text);
+                x = Convert.ToDouble(x_textbox_.Text);
+
+                polyminal = new Polyminal(a, b, c, d);
+
+                ans_polyminal_with_x.Content = polyminal.evaluate(x);
+                polyminal_look_for_x.Content = polyminal.output_polyminal();
+
+            }
+
+            catch
+            {
+                MessageBox.Show("Помилка");
+                return;
+            }
+        }
+
+        private void answer_random_diapaz_Click(object sender, RoutedEventArgs e)
+        {
+            Polyminal first_polyminal, second_polyminal, answer_polyminal;
+
+            int min, max;
+            double x;
+
+            try
+            {
+                min = Convert.ToInt32(min_diapzon.Text);
+                max = Convert.ToInt32(max_diapzon.Text);
+                x = Convert.ToDouble(texbox_for_x_diapaz.Text);
+                first_polyminal = new Polyminal(min, max);
+                second_polyminal = new Polyminal(min, max);
+                answer_polyminal = first_polyminal.adding(second_polyminal);
+
+                first_polyminal_label.Content = first_polyminal.output_polyminal();
+                second_polyminal_label.Content = second_polyminal.output_polyminal();
+                answer_polyminal_label.Content = answer_polyminal.output_polyminal();
+
+                if (x != 0)
+                {
+                    answer_for_x_diapaz.Content = answer_polyminal.evaluate(x);
+                } 
+        
+            }
+            catch
+            {
+                MessageBox.Show("Помилка");
+                return;
+            }
+        }
+
+        private void fix_diapaz_calculate_Click(object sender, RoutedEventArgs e)
+        {
+            Polyminal first_polyminal, second_polyminal, answer_polyminal;
+            double x;
+
+            try
+            {
+                first_polyminal = new Polyminal();
+                second_polyminal = new Polyminal(); 
+                x = Convert.ToDouble(third_x.Text);
+                answer_polyminal = first_polyminal.substract(second_polyminal);
+               
+
+
+                first_polyminal_fix_diapz.Content = first_polyminal.output_polyminal();
+                second_polyminal_fix_diapz.Content = second_polyminal.output_polyminal();
+                answer_polyminal_fix_diapz.Content = answer_polyminal.output_polyminal();
+                
+                if (x != 0)
+                {
+                    ans_with_x_polyminal_fix_diapz.Content = answer_polyminal.evaluate(x);
+                }
+                
+            }
+
+            catch
+            {
+                MessageBox.Show("Помилка");
+                return;
+            }
+        }
+    }
+}
---
+++ WpfApp1/Polyminal.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Polyminal
+    {
+
+        private double _a, _b, _c, _d;
+
+        // Констурктор для введення із клави
+        public Polyminal(double a, double b, double c, double d)
+        {
+            _a = a;
+            _b = b;
+            _c = c;
+            _d = d;
+        }
+        // Генератор рандому
+        private void random(int min, int max)
+        {
+            Random rand = new Random();
+            _a = rand.Next(min, max);
+            _b = rand.Next(min, max);
+            _c = rand.Next(min, max);
+            _d = rand.Next(min, max);
+        }
+
+        // Конструктор для рандому
+        public Polyminal(int min, int max) 
+        {
+            random(min, max);
+        }
+
+        // Конструктор для рандому у діапазоні від 0 до 100
+
+        public Polyminal()
+        {
+            random(0, 100);
+        }
+
+        // Обчислення значення многочлена
+        public double evaluate(double x)
+        {
+            return _a * Math.Pow(x, 3) + _b * Math.Pow(x, 2) + _c * x + _d;
+        }
+
+        // Додавання многочлена 
+
+        public Polyminal adding(Polyminal second_polyminal)
+        {
+            double answer_polyminal_a = this._a + second_polyminal._a;
+            double answer_polyminal_b = this._b + second_polyminal._b;
+            double answer_polyminal_c = this._c + second_polyminal._c;
+            double answer_polyminal_d = this._d + second_polyminal._d;
+
+            return new Polyminal(answer_polyminal_a, answer_polyminal_b, answer_polyminal_c, answer_polyminal_d);
+
+        }
+
+        // Віднімання многочлена
+
+        public Polyminal substract (Polyminal second_polyminal)
+        {
+            double answer_polyminal_a = this._a - second_polyminal._a;
+            double answer_polyminal_b = this._b - second_polyminal._b;
+            double answer_polyminal_c = this._c - second_polyminal._c;
+            double answer_polyminal_d = this._d - second_polyminal._d;
+
+            return new Polyminal(answer_polyminal_a, answer_polyminal_b, answer_polyminal_c, answer_polyminal_d);
+        }
+
+        // Виведення многочлена
+
+        public string output_polyminal()
+        {
+            return $"{_a}x^3 + {_b}x^2 + {_c}x + {_d}";
+        }
+    }
+}
